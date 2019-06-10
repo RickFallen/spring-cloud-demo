8db346640bbb19180836ef2027325cccb3d7a09d
Wu Ricky <403519557@qq.com>
2019-06-11 00:04:01 +0800

    初始化zuul

--- student-service-consumer/src/main/java/com/kuaibao/student/controller/StudentController.java
+++ student-service-consumer/src/main/java/com/kuaibao/student/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.kuaibao.student.controller;
 
 import com.kuaibao.student.consumer.StudentServcie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,13 @@ public class StudentController {
     @Autowired
     StudentServcie studentServcie;
 
+    @Value("${server.port}")
+    private Integer port;
+
     @RequestMapping(value = "/student" , method = RequestMethod.GET)
     public String getStudent(){
-        return "student name is :" + studentServcie.getName() + ",age is :" + studentServcie.getAge();
+        return "student name is :" + studentServcie.getName() + ",age is :" + studentServcie.getAge() + "\n" +
+                "from consumer port :" + port;
     }
 
     @RequestMapping(value = "/test" , method = RequestMethod.GET)
---
+++ zuul-service/src/main/java/com/kuaibao/zuul/config/FallBackProvider.java
@@ -0,0 +1,70 @@
+package com.kuaibao.zuul.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class FallBackProvider implements FallbackProvider {
+    @Value("${zuul.routes.student.serviceId}")
+    private String STUDENT_ROUTE;
+
+    @Override
+    public String getRoute() {
+        return STUDENT_ROUTE;
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse(){
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+                return httpHeaders;
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("status", "500");
+                jsonObject.put("message", "无法连接网络");
+                return new ByteArrayInputStream(jsonObject.toJSONString().getBytes(Charset.defaultCharset()));
+            }
+
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.OK.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.OK.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+    }
+}

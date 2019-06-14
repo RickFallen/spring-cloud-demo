89094e97785995c6afb2732a290e8ad520acb8ef
吴承甫 <wucf@dtinsure.com>
2019-06-14 13:55:50 +0800

    更改项目名

--- gateway/src/main/java/com/kuaibao/zuul/GatewayApplication.java
+++ gateway/src/main/java/com/kuaibao/zuul/GatewayApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ZuulServiceApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ZuulServiceApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
